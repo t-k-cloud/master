66cf108cebed7b9af490955870b5a5d52d2f7ece
t-k- <clock126@126.com>
2017-08-30 16:59:38 -0400

    fixes to previous commit

--- jobs/discover-and-sync.sh
+++ jobs/discover-and-sync.sh
@@ -6,7 +6,7 @@ MAC1=$CLIENT_MAC1
 MAC2=$CLIENT_MAC2
 CLIENT_IPV6=''
 PORT=$CLIENT_RSYNC_PORT
-NET_INT_FACE=NETWORK_INTERFACE
+NET_INT_FACE=$MASTER_NETWORK_INTERFACE
 LOCKFILE=discover-and-sync.lock
 
 # check .lock file
@@ -16,17 +16,32 @@ fi;
 
 # discover neighbors
 echo "discovering $MAC1 or $MAC2 from interface $NET_INT_FACE"
-ping -6 -c 3 'ff02::1%'$enp0s20u1
+ping -6 -c 3 'ff02::1%'$NET_INT_FACE
 
 
-if ip neigh | grep $MAC1; then
-	CLIENT_IPV6= `ip neigh | grep $MAC1 | awk '{print $1}'`
-else if ip neigh | grep $MAC2; then
-	CLIENT_IPV6= `ip neigh | grep $MAC2 | awk '{print $1}'`
-else
-	# echo 'Client not connected to local network, abort.'
-	exit 0;
-fi
+while true; do
+	if ip -6 neigh | grep $MAC1; then
+		echo "MAC1 found in cache"
+		CLIENT_IPV6=`ip -6 neigh | grep $MAC1 | awk '{print $1}'`
+		MAC1="impossible"
+	elif ip -6 neigh | grep $MAC2; then
+		echo "MAC2 found in cache"
+		CLIENT_IPV6=`ip -6 neigh | grep $MAC2 | awk '{print $1}'`
+		MAC2="impossible"
+	else
+		echo 'Client not connected to local network, abort.'
+		exit 0;
+	fi
+
+	echo "Is you there??? ${CLIENT_IPV6}%${NET_INT_FACE}"
+	if ping -6 -c 3 ${CLIENT_IPV6}%${NET_INT_FACE}; then
+		echo OOOOOOOOOOO
+		exit 0;
+	else
+		echo XXXXXXXXXXX
+		exit 0;
+	fi;
+done
 
 # before start, create .lock file
 echo "client found: $CLIENT_IPV6 (port $PORT), starting rsync ..."
