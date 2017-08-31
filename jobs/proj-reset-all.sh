df19236732e301b26e349a17243de5956f733ae8
t-k- <clock126@126.com>
2017-08-30 23:51:42 -0400

    fix proj-reset-all.sh script

--- jobs/proj-reset-all.sh
+++ jobs/proj-reset-all.sh
@@ -4,6 +4,7 @@ source $curdir/env.cfg
 
 find "$MASTER_TREE_PATH/proj" -name .git -type d -prune -print0 | while read -d $'\0' gitdir; do
 	repo_dir=$(cd $gitdir && cd .. && pwd)
+	cd $repo_dir && pwd
 	echo "mirror $repo_dir"
 	git fetch --all
 	git reset --hard origin/master
