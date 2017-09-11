8390b216591fb642b062881f2ee03abd90940159
t-k- <clock126@126.com>
2017-09-11 01:38:59 -0400

    add printing

--- jobs/droppy-sshtunnel-keepalive.sh
+++ jobs/droppy-sshtunnel-keepalive.sh
@@ -3,6 +3,7 @@ curdir=$(cd $(dirname $0) && pwd)
 source $curdir/env.cfg
 
 while true; do
+	echo "Establishing remote ssh tunnel ..."
 	ssh $SSH_TUNNEL_OPTS -NR 8991:localhost:8991 $SSHTO
 	echo "Remote SSH tunnel closed, reopen it ..."
 	sleep 3
