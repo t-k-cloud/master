685ecd42d1e72b43dcce634000912d11f624e3fa
t-k- <clock126@126.com>
2018-05-29 00:34:21 -0400

    mysql setup

---
+++ jobs/mysql-add-tkblog-usr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source $curdir/env.cfg
+
+mysql -u root --password=$MYSQL_PASSWD << EOF
+CREATE USER 'thoughts_ga6840'@'localhost' IDENTIFIED BY 'xxxxxxxxxxxxx';
+create database thoughts_ga6840;
+GRANT ALL PRIVILEGES ON thoughts_ga6840.* TO 'thoughts_ga6840'@'%' IDENTIFIED BY 'xxxxxxxxxxxxx';
+EOF
+
+echo "always return 0 for this script."
