37902314fca59e4fbe0dad6bf35f4d9612243ade
t-k- <clock126@126.com>
2018-07-17 00:43:11 -0400

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,6 @@ fi
 tput setaf 6 # colorful
 echo "Now: cd $MASTER_TREE_PATH/$PROJ_PATH/jobd"
 echo "Start jobd: sudo node ./jobd.js $USER ../master/jobs --bootstrap"
-echo "Run job nginx:nginx-setup to setup nginx virtual host."
+echo "Run job nginx:setup to setup nginx virtual host."
 echo -n "After http://localhost/auth/login is accessble, "
 echo "Start authd.js and restart jobd without --bootstrap option."
--- jobs/mysql-add-tkblog-usr.sh
+++ jobs/mysql-add-tkblog-usr.sh
@@ -2,7 +2,7 @@
 curdir=$(cd $(dirname $0) && pwd)
 source $curdir/env.cfg
 
-mysql -u root --password=$MYSQL_PASSWD << EOF
+mysql -u root --password="$MYSQL_PASSWD" << EOF
 CREATE USER 'thoughts_ga6840'@'localhost' IDENTIFIED BY 'xxxxxxxxxxxxx';
 create database thoughts_ga6840;
 GRANT ALL PRIVILEGES ON thoughts_ga6840.* TO 'thoughts_ga6840'@'%' IDENTIFIED BY 'xxxxxxxxxxxxx';
